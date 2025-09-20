13273a7d3170b9a5b353e5b3867b6203713950b1
Priyam Jaiswal <priyamj608@gmail.com>
2025-09-20 14:00:00 +0530

    Add array challenges 27-30

---
+++ src/t23To30/arrayChallenge29.java
@@ -0,0 +1,25 @@
+package t23To30;
+
+public class arrayChallenge29 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Palindrome Checker Array\n");
+        int[] numArr = ArrayUtility.inputArray();
+        boolean isPalin = isPalindrome(numArr);
+        if(isPalin){
+            System.out.println("Your array is Palindrome");
+        } else {
+            System.out.println("Your array is not Palindrome");
+        }
+    }
+
+    public static boolean isPalindrome(int[] numArr){
+        int i = 0;
+        while(i < numArr.length / 2){
+            if(numArr[i] != numArr[numArr.length - 1 - i]){
+                return false;
+            }
+            i++;
+        }
+        return true;
+    }
+}
