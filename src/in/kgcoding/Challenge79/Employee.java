ce7db3cdaba54c1eeecd544eb868a2ecc672402e
Priyam Jaiswal <priyamj608@gmail.com>
2025-10-27 10:45:00 +0530

    Add OOP challenges 78-79: bank account and employee

---
+++ src/in/kgcoding/Challenge79/Employee.java
@@ -0,0 +1,99 @@
+package in.kgcoding.Challenge79;
+
+public class Employee {
+
+    private String name;
+    private int age;
+    private double salary;
+
+    Employee(String name, int age, double salary) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    String getEmployeeDetails() {
+        return "EmployeeDetails: Name:" + name
+                + ", Age:" + age
+                + ", Salary:" + salary;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/in/kgcoding/Challenge79/TestEmployee.java
@@ -0,0 +1,9 @@
+package in.kgcoding.Challenge79;
+
+public class TestEmployee {
+    public static void main(String[] args) {
+        Employee emp = new Employee("Priyam", 30, 25000);
+        emp.setName("shivam");
+        System.out.println(emp.getEmployeeDetails());
+    }
+}
