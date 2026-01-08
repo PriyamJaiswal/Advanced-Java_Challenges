15b791ce56e19a19f3a527e3d97f91aaab1b73a8
Priyam Jaiswal <priyamj608@gmail.com>
2026-01-08 14:00:00 +0530

    Add threading challenge 99: HelloThread

---
+++ src/in/kgcoding/Challenge99/main.java
@@ -0,0 +1,12 @@
+package in.kgcoding.Challenge99;
+
+public class main {
+    public static void main(String[] args) {
+        HelloThread t1 = new HelloThread(1);
+        HelloThread t2 = new HelloThread(2);
+
+        t1.start();
+        t2.start();
+
+    }
+}
